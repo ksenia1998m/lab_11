bc119a7c6e9dfb38a03a986d828b3606ccfdc9e3
Ksenia1998 <st_k.v.malionovskaya@mpt.ru>
2019-12-27 01:07:39 +0300

    измение

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,9 @@ import { TouchSequence } from 'selenium-webdriver';
 
 export class AppComponent {
   sensors: Sensor[] = [];
+  public i : number;
   constructor() {
-    for(let i=0; i < 10; i++) {
+    for(let i=0; i <= 9; i++) {
       this.sensors.push(new Sensor(i, `Датчик № ${i}`));  
     }
   }
@@ -20,7 +21,23 @@ export class AppComponent {
     this.sensors.splice(idx, 1);
   }
 
-  onCreateSensor(new_name, new_status) {
-    this.sensors.push(new Sensor(this.sensors[this.sensors.length - 1].id + 1, new_name.value, new_status.checked));
-  }
-}
+ 
+  onCreateSensor(new_name, new_status) { 
+    let n; 
+    if (new_name.value === ' '  || new_name.value === null){ 
+    return false; 
+    } else{
+    if (this.sensors.length == 0){ 
+    n = 1; this.sensors.push(new Sensor(n, new_name.value, new_status.checked));
+    } 
+    else{ 
+    n =this.sensors[this.sensors.length - 1].id + 1; this.sensors.push(new Sensor(n, new_name.value, new_status.checked));
+    }} 
+    } 
+    
+  
+    } 
+
+    
+
+ 
